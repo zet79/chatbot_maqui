63588493b56be212531a72f7a40bc1f9b74b11b7
Daniel Ernesto Rivas Pareja <eselrey551@gmail.com>
2024-11-04 23:11:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -532,4 +532,4 @@ if __name__ == '__main__':
 
     #threading.Thread(target=verificar_estados_clientes).start()
     # Iniciar la aplicación Flask
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000)
